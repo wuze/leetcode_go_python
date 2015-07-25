0c083a2f78d29ddd7ef1d16649c92425bf157497
forest <run_forest@163.com>
2015-07-25 13:29:37 +0800

    a

--- go/addBinary/addBanary.go
+++ go/addBinary/addBanary.go
@@ -7,7 +7,6 @@
  * b = "1"
  * Return "100".
  *
- *
  **********************************************************************************/
 
 package main
@@ -21,32 +20,56 @@ func addTwoNum(a, b string) (ret string) {
 
 	alen := len(a)
 	blen := len(b)
-	carray := 0
+	cbit := 0
+	var carry bool
 
 	for alen > 0 || blen > 0 {
 		abit := 0
 		bbit := 0
+
 		if alen <= 0 {
 			abit = 0
 		} else {
-			abit = a[alen-1] - '0'
+			abit = int(a[alen-1]) - 48
 		}
+
 		if blen <= 0 {
 			bbit = 0
 		} else {
-			bbit = b[blen-1] - '0'
+			bbit = int(b[blen-1]) - 48
 		}
 
-		tmp := abit + bbit + carray
-		c = strconv.Itoa(tmp&1) + c
+		ret = strconv.Itoa((abit+bbit+cbit)&1) + ret
 
+		carry = (abit + bbit + cbit) > 1
+
+		if carry {
+			cbit = 1
+		} else {
+			cbit = 0
+		}
+
+		fmt.Println("--", abit, bbit, cbit, "--")
+
+		alen--
+		blen--
+	}
+
+	if carry {
+		cbit = 1
+	} else {
+		cbit = 0
+	}
+
+	if cbit == 1 {
+		ret = "1" + ret
 	}
 	return
 }
 
 func main() {
 	a := "101"
-	b := "1201"
+	b := "1101"
 	c := addTwoNum(a, b)
 	fmt.Printf("%s + %s = %s\n", a, b, c)
 }
