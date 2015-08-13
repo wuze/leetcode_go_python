5bf537e84b939352d719e158e936364551a18fab
lishiwu <lishiwu@xiaomi.com>
2015-08-13 17:55:11 +0800

    约瑟夫

---
+++ python/jsph/j.py
@@ -0,0 +1,23 @@
+#!/bin/bash python
+#encoding:utf-8
+
+
+
+def displayRet(num,total):
+    print "Total: ", total ," Num:",num 
+    print  range(1, total+1)
+
+    if total==1:
+        print 1
+    arr=range(1,total+1)
+
+    while len(arr)>0:
+        for i in range(num-1):
+            arr.append(arr.pop(0))
+        print arr.pop(0), ", ",
+
+
+if __name__=="__main__":
+    total=10
+    num=5
+    displayRet(num,total)
